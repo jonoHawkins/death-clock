98a13f9a573f663e8a6790537e8dd9bc44613805
jon <jon@wordery.com>
2020-03-03 14:43:27 +0000

    added Ash 🥺

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import DeathCountdown from './DeathCountdown';
 
 function App() {
-  const calculateTimeLeft = () => {
-    const difference = (new Date("2020-02-28 17:30")) - (new Date());
-    let timeLeft = {};
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60)
-      };
-    }
-
-    return timeLeft;
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-  useEffect(() => {
-    setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-  });
-
-  const timerComponents = [];
-
-  Object.keys(timeLeft).forEach((interval, i) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
-
-    timerComponents.push(
-      <span key={i}>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
-    );
-  });
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <span className="App-logo">☠️</span>
-        <p>{timerComponents.length ? <>Dan you are dead to us in<br /> {timerComponents}</> : <span>Dan you're dead to us now. Good luck.</span>}</p>
-      </header>
-    </div>
+    <DeathCountdown
+      colleagueName="Ash"
+      end="2020-05-15 17:30"
+    />
   );
 }
 
---
+++ src/DeathCountdown.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+function DeathCountdown({ end, colleagueName }) {
+    const calculateTimeLeft = () => {
+        const difference = (new Date(end)) - (new Date());
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60)
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+    }, []);
+
+    const timerComponents = [];
+
+    Object.keys(timeLeft).forEach((interval, i) => {
+        if (!timeLeft[interval]) {
+            return;
+        }
+
+        timerComponents.push(
+            <span key={i}>
+                {timeLeft[interval]} {interval}{" "}
+            </span>
+        );
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <span className="App-logo">☠️</span>
+                <p>{timerComponents.length ? <>{colleagueName} you are dead to us in<br /> {timerComponents}</> : <span>{colleagueName} you're dead to us now. Good luck.</span>}</p>
+            </header>
+        </div>
+    );
+}
+
+export default DeathCountdown;
